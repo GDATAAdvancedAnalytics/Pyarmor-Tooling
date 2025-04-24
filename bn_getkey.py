72118fca1746bc4242c1436f26d7823309bc7b56
estr3llas <nao24nao69nao420@gmail.com>
2025-04-24 09:08:35 -0300

    Changed comments to an actual BN example.

--- bn_getkey.py
+++ bn_getkey.py
@@ -7,17 +7,20 @@ PYARMOR_STRING = b"pyarmor-vax-007106\x00\x00"
 
 # References to these are in the "get_key_via_md5" function
 """
-  md5_process(
-    v6,
-    (char *)&unk_64944060 + g_dword_64944050_0x20_rsaoffset,
-    (unsigned int)g_dword_64944054_0x10E_rsakeylen);// rsa key
+  get_key_via_md5(&var_1c8, sx.q(data_65642050) + &data_65642060, data_65642054)
 """
-INFO_BLOB_ADDR = 0x64944060
+INFO_BLOB_ADDR = 0x65642060
+
 # First xmmword that is xored:
-# xmmword_64948140 = (__int128)_mm_xor_si128(_mm_load_si128((const __m128i *)&xmmword_64948140), si128);
-RSA_KEY2_ADDR = 0x64948140
+# 	int128_t zmm0 = data_656479c0
+#	data_65646140 ^= zmm0
+#	data_65646150.o ^= zmm0
+#	data_65646160 ^= zmm0
+RSA_KEY2_ADDR = 0x65646140
+
 # From a global dword passed to md5_process (NOTE: we assume size is the same for rsakey and rsakey2, validate this!)
 RSA_KEY_SIZE = 0x10E
+
 # Byte value that RSA_KEY2 is xored with
 RSA_XOR_KEY = 0xF1
 
